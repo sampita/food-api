590589791a2df26da1a3861bd9ae2d53bc922728
Sam Pita <samanthakpita@gmail.com>
2019-10-15 16:48:53 -0500

    answered practice, beginning styling

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,27 +1,60 @@
-console.log("wassssup")
+// console.log("wassssup")
 
-// .then means AFTER the thing prior loads first, THEN
+// // .then means AFTER the thing prior loads first, THEN
+
+
+// //making a call to database.json
+// fetch("http://localhost:8088/food")
+//     .then(foods => {
+//         console.log("foods", foods)
+//         //parse results of fetch request to json/javascript
+//         return foods.json()
+//     })
+
+//     .then(parsedFoods => {
+//         // display json data in console table
+//         console.table(parsedFoods)
+//     });
+
+
+
+
+//     // // as a normal function
+//     // .then(parsedFoods => {
+//     //     console.table(parsedFoods)
+//     // })
+
+//     // .then(function (taco) {
+//     //     console.table(taco)
+//     // });
+
+
+// Practice: Displaying Foods
+
+const foodFactory = (obj) => {
+    return `
+    <div class=>
+    <h1>${obj.name}</h1>
+    <p>${obj.category}</p>
+    <p>${obj.ethnicity}</p>
+    </div>`
+}
+
+let foodInput = ""
+const addFoodToDom = (food) => {
+    foodInput += food;
+    console.log(foodInput)
+    document.querySelector(".foodList").innerHTML = foodInput;
+}
 
 fetch("http://localhost:8088/food")
-    .then(foods => {
-        console.log("foods", foods)
-        //parse results of fetch request to json/javascript
-        return foods.json()
-    })
-
+    //parse fetch request results to javascript
+    .then(foods => foods.json())
     .then(parsedFoods => {
-        // display json data in console table
-        console.table(parsedFoods)
-    });
+        parsedFoods.forEach(food => {
+            // console.log(food);
+            const foodAsHTML = foodFactory(food)
+            addFoodToDom(foodAsHTML)
+        })
 
-
-
-
-    // // as a normal function
-    // .then(parsedFoods => {
-    //     console.table(parsedFoods)
-    // })
-
-    // .then(function (taco) {
-    //     console.table(taco)
-    // });
+    })
